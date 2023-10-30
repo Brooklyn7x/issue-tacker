b91712a015bdaeabb0f20b596121d884b5175efc
Brooklyn7x <shubham.jaiswal020@gmail.com>
2023-10-30 13:53:25 +0530

    styling

---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const LoadingNewPage = () => {
+    return (
+        <div>Loading...</div>
+    )
+}
+
+export default LoadingNewPage
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,30 +1,40 @@
 'use client'
-import { TextArea, TextFieldInput, TextFieldRoot, Button, Callout } from '@radix-ui/themes'
+import { TextArea, TextFieldInput, TextFieldRoot, Button, Callout, Text } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import "easymde/dist/easymde.min.css";
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/createIssueSchema';
+import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 
-
-interface IssueForm {
-    title: string;
-    description: string
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
+// interface IssueForm {
+//     title: string;
+//     description: string
+// }
 
 
 
 const NewIssuePage = () => {
     const router = useRouter();
-    const { register, control, handleSubmit } = useForm<IssueForm>();
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    });
     const [error, setError] = useState('')
+    const [issumit, setIsSumit] = useState(false)
     const onSubmit = async (data: IssueForm) => {
         try {
+            setIsSumit(true)
             await axios.post('/api/issues', data);
             router.push('/issues');
         } catch (error) {
+            setIsSumit(false)
             setError('SomeThing Wrong.')
         }
     };
@@ -37,12 +47,14 @@ const NewIssuePage = () => {
                 <TextFieldRoot>
                     <TextFieldInput placeholder='Title' {...register('title')} />
                 </TextFieldRoot>
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
                 <Controller
                     name="description"
                     control={control}
                     render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
                 />
-                <Button>Sumbit New Issue</Button>
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
+                <Button disabled={issumit}>Sumbit New Issue{issumit && <Spinner />}</Button>
             </form>
         </div>
     )
