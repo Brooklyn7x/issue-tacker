e47341cc21452840f345d592a25e11a21ede2dc7
Brooklyn7x <shubham.jaiswal020@gmail.com>
2023-11-02 15:27:31 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import LastestIssues from './LastestIssues'
 import IssuesSummary from './IssuesSummary'
 import prisma from '@/prisma/client'
 import IssueCharts from './IssueCharts'
+import { Flex, Grid } from '@radix-ui/themes'
+import { Metadata } from 'next'
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: 'OPEN' } })
@@ -11,7 +13,17 @@ export default async function Home() {
   const inProgress = await prisma.issue.count({ where: { status: 'IN_PROGRESS' } })
 
   return (
-    // <LastestIssues />
-    <IssueCharts open={open} inProgress={inProgress} closed={closed} />
+    <Grid columns={{ initial: '1', md: "2" }} gap="5" >
+      <Flex direction='column' gap='5'>
+        <IssuesSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueCharts open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LastestIssues />
+    </Grid>
   )
+}
+
+export const metadata: Metadata = {
+  title: 'Issue Tacker - Dashboard',
+  description: 'View a summary of project issues'
 }
